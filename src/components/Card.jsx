1db72668eab8ab225ad71427d91ab842507ada29
nithin9066 <nithin9066@gmail.com>
2025-03-27 13:58:49 +0530

    fixed bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,12 +8,12 @@ export default function Card({ data, type }) {
     const toUrl = isCategory ? `/recipies/${data.strCategory}` : `/recipie/${data.idMeal}`;
     const imageUrl = isCategory ? data.strCategoryThumb : data.strMealThumb
     return (
-        <div className="p-4">
+        <Link to={toUrl} className="p-4">
             <div className={`${isCategory ? 'flex-col justify-center' : 'flex-row'} flex items-center`}>
                 <div className="flex-shrink-0">
-                    <Link to={toUrl} className="relative block">
+                    <div className="relative block">
                         <LazyLoadImage alt="profil" loading="lazy" src={imageUrl} className={`mx-auto object-cover rounded-${isCategory ? 'full' : 'md'} h-20 w-20 `} />
-                    </Link>
+                    </div>
                 </div>
                 <div className="mt-2 text-center flex flex-col w-full">
                     <p className="text-lg font-medium text-gray-600 dark:text-white">
@@ -21,7 +21,7 @@ export default function Card({ data, type }) {
                     </p>
                 </div>
             </div>
-        </div>
+        </Link>
 
     )
 }
